6d1ee2aa56030d6380a20704991adc42b9dd95ba
Alawi AlQurashi <126424764+AlawiAlQurashi@users.noreply.github.com>
2023-08-16 01:04:50 +0300

    Add files via upload

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,56 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🗒 الاوامر",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="المساعدة", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="اضغط لاضافتي لمجموعتك✅",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🗒 الاوامر", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="ᯓ𝐘𝐄 ||✘𝐔𝐏𝐏𝐄𝐑 •🇾🇪⇩𓄹 ]ِ", url=f"https://t.me/UPE3R"
+            ),
+            InlineKeyboardButton(
+                text="👤 مطور البوت", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=" ⌞ 𝐀𝐖 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝ ", url=f"https://t.me/AWCODE3"
+            )
+        ],
+     ]
+    return buttons
